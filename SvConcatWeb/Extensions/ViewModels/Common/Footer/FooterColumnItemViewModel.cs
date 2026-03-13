3dcb4fe086bf2f424edda9b70c775f175ff74b7c
Max Bongers <maxbongers321@proton.me>
2026-03-13 21:32:45 +0100

    Created footer

--- SvConcatWeb/Extensions/ViewModels/Common/Footer/FooterColumnItemViewModel.cs
+++ SvConcatWeb/Extensions/ViewModels/Common/Footer/FooterColumnItemViewModel.cs
@@ -5,4 +5,8 @@ public class FooterColumnItemViewModel
     public ImageItemViewModel Icon { get; set; }
     public LinkViewModel Link { get; set; }
     public string Text { get; set; }
+    
+    public bool HasIcon => Icon?.Url != null;
+    public bool HasLink => Link?.Url != null;
+    public bool HasText => !string.IsNullOrWhiteSpace(Text);
 }
